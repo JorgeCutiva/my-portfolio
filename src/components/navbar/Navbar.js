52b7da1ceeb5470fb1f655ff19c394d05b90c190
Jorge Cutiva <jorgeandres1461@gmail.com>
2022-05-29 02:37:41 -0500

    Create file component of Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const activeStyle = {
+    color: 'red'
+  }
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            HomePage
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about-me"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            AboutMe
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
